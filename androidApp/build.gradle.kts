f674230c7d150428823188778180ad21e5a676bf
arthur mikulski <a.mikulski@reply.it>
2025-02-03 21:27:25 +0100

    Added skie plugin to shared module build gradle, removed unused imports inside ArticlesViewModel

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.touchlab.skie)
 }
 
 android {
--- shared/src/commonMain/kotlin/com/example/kmp/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/kmp/articles/ArticlesViewModel.kt
@@ -1,15 +1,11 @@
 package com.example.kmp.articles
 
 import com.example.kmp.BaseViewModel
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
 
 class ArticlesViewModel(
     private val useCase: ArticlesUseCase

cc8f2257ae35f99744f24ab62b6ad257b7fa6441
Arthur <arthur.from.rivia@gmail.com>
2025-02-03 10:48:14 +0100

    Added iOS Screens and other implementation

---
+++ shared/src/iosMain/kotlin/com/example/kmp/di/KoinInitializer.kt
@@ -0,0 +1,19 @@
+package com.example.kmp.di
+
+import com.example.kmp.articles.ArticlesViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+fun initKoin() {
+
+    val modules = sharedKoinModules
+    startKoin { modules(modules) }
+
+}
+
+class ArticlesInjector: KoinComponent {
+
+    val articlesViewModel: ArticlesViewModel by inject()
+
+}
